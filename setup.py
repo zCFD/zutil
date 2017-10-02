f5b75f8e211c497ec12f6b9e4fa7c007436bb302
James Sharpe <james.sharpe@zenotech.com>
2017-10-02 10:39:30 +0100

    Adding MANIFEST file

--- setup.py
+++ setup.py
@@ -1,10 +1,10 @@
 from distutils.core import setup
 
 setup(name="zutil",
-      version=0.1,
+      version='0.1',
       description="Utilities used for generating zCFD control dictionaries",
       author="Zenotech",
       author_email="support@zenotech.com",
       url="https://zcfd.zenotech.com/",
-      packages=["zutil",],
+      packages=["zutil"],
       )
