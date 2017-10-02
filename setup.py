c822ed90776748069f6ace084a1b8ca43c7397ec
James Sharpe <james.sharpe@zenotech.com>
2017-10-02 11:32:10 +0100

    Add missing files to package

--- setup.py
+++ setup.py
@@ -1,10 +1,20 @@
 from distutils.core import setup
 
 setup(name="zutil",
-      version='0.1',
+      version='0.1.1',
       description="Utilities used for generating zCFD control dictionaries",
       author="Zenotech",
       author_email="support@zenotech.com",
       url="https://zcfd.zenotech.com/",
-      packages=["zutil"],
+      packages=["zutil", "zutil.post", "zutil.analysis", "zutil.plot"],
+      install_requires=[
+          'mpi4py',
+          'ipython',
+          'Fabric',
+          'ipywidgets',
+          'matplotlib',
+          'numpy',
+          'pandas',
+          'PyYAML'
+        ],
       )
