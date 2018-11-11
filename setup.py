137d3c0394309dfb22a407eda5b80bc312482c1d
James Sharpe <james.sharpe@zenotech.com>
2018-11-11 12:25:47 +0000

    Add license and Source Code url

--- setup.py
+++ setup.py
@@ -5,7 +5,11 @@ setup(name="zutil",
       description="Utilities used for generating zCFD control dictionaries",
       author="Zenotech",
       author_email="support@zenotech.com",
+      license="MIT",
       url="https://zcfd.zenotech.com/",
+      project_urls={
+          "Source Code": "https://github.com/zCFD/zutil/",
+      },
       packages=["zutil", "zutil.post", "zutil.analysis", "zutil.plot"],
       install_requires=[
           'ipython<6.0',
