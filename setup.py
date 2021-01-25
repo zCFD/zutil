bb26564fac600125b7435a2966de774d85c9a1ec
James Sharpe <james.sharpe@zenotech.com>
2021-01-25 11:13:04 +0000

    Bump version

--- setup.py
+++ setup.py
@@ -13,7 +13,7 @@ classes = """
 classifiers = [s.strip() for s in classes.split('\n') if s]
 
 setup(name="zutil",
-      version='1.0.2',
+      version='1.0.3',
       description="Utilities used for generating zCFD control dictionaries",
       author="Zenotech",
       author_email="support@zenotech.com",
