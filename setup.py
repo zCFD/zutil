be7baea27d25935e0345eec32c8534a22e5541f1
Mike Turner <mike.turner@zenotech.com>
2023-09-13 10:33:30 +0100

    updates to setup.py for publishing

--- setup.py
+++ setup.py
@@ -1,38 +1,8 @@
-from distutils.core import setup
+#!/usr/bin/env python
+import os
+import setuptools
 
-classes = """
-    Development Status :: 4 - Beta
-    Intended Audience :: Developers
-    License :: OSI Approved :: MIT License
-    Topic :: System :: Logging
-    Programming Language :: Python
-    Programming Language :: Python :: 3
-    Programming Language :: Python :: 3.7
-    Operating System :: POSIX :: Linux
-"""
-classifiers = [s.strip() for s in classes.split("\n") if s]
+version = os.environ.get('RELEASE_VERSION', '0.0.0')
 
-setup(
-    name="zutil",
-    version="1.0.3",
-    description="Utilities used for generating zCFD control dictionaries",
-    author="Zenotech",
-    author_email="support@zenotech.com",
-    license="MIT",
-    url="https://zcfd.zenotech.com/",
-    classifiers=classifiers,
-    project_urls={"Source Code": "https://github.com/zCFD/zutil/"},
-    packages=["zutil", "zutil.post", "zutil.analysis", "zutil.plot"],
-    install_requires=[
-        "future",
-        "ipython",
-        "fabric>=2.5",
-        "ipywidgets",
-        "matplotlib",
-        "numpy",
-        "pandas",
-        "PyYAML",
-        "dill",
-    ],
-    extras_require={"mpi": ["mpi4py"]},
-)
+if __name__ == "__main__":
+    setuptools.setup(version=version)
